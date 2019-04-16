c5f75259f19ab3929b2778af1b9dd63727f7e917
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-04-16 11:05:59 -0700

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
-// Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
+  onClick = () => {
+    this.setState({
+      mood: 'sad'
+    })
+  }
+
+
+  render() {
+    return (
+      <div onClick={this.onClick}>{this.state.mood}</div>
+    )
+  }
+}
